5c91deb7e760584c7195a74ed50585b2adb87d18
stephcerratti <stephanie.cerratti@gmail.com>
2017-03-14 23:57:07 -0400

    #01: rough draft
    rough draft of functions,
    index.html for initial testing of functionality.

---
+++ app.js
@@ -0,0 +1,44 @@
+//this will add a 5% markup to the initial base price.
+
+let basePrice = 
+let currentSubtotal = 0;
+
+addBaseMarkup() {
+	 currentSubtotal = basePrice * 1.05;
+	console.log(currentSubtotal);
+	return currentSubtotal;
+}
+
+//return currentSubtotal
+
+//this is give the total markup based on the number of people working on the job (1.2% for every person)
+let numberOfPeople = 
+addPersonMarkup()
+	let personMarkup = numberOfPeople * 1.12;
+	console.log(personMarkup);
+	return personMarkup;
+
+
+//this will add the person on the job markup + the category markup. 
+additionalMarkup()
+	if (categoryPharmacy) {
+		personMarkup + 7;
+	}
+
+	if (categoryFood) {
+
+	}
+
+	if (categoryElectronics) {
+
+	}
+
+	else {
+
+	}
+//return newMarkup
+
+//this will take the currentSubtotal and apply the newMarkup, whith will return the total price of
+//the job. 
+newTotal()
+
